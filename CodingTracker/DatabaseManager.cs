88e9fd6d5f440d21306d9e6afaf372ed9ece65b4
Václav Machovský <vmach.projects@gmail.com>
2023-04-04 13:16:29 +0200

    Add project files.

---
+++ CodingTracker/DatabaseManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace CodingTracker
+{
+    internal class DatabaseManager
+    {
+        public void CreateTable(string connectionString)
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS codingTracker (
+                                            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                            Date TEXT,
+                                            Duration TEXT
+                                        )";
+
+                    tableCmd.ExecuteNonQuery();
+                }
+            }
+            Console.WriteLine(".");
+        }
+        
+    }
+}
---
+++ CodingTracker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+
+namespace CodingTracker
+{
+    internal class Program
+    {
+        static string connectionString = ConfigurationManager.AppSettings.Get("ConnectionString");
+        static void Main(string[] args)
+        {
+            DatabaseManager databaseManager = new();
+
+            databaseManager.CreateTable(connectionString);
+        }
+    }
+}
